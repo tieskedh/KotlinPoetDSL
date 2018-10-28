76f66c1640b7a5100842c788549ddd10527c0b2a
Thijs de Haan <thijsdehaan71@hotmail.com>
2018-10-28 16:50:28 +0100

    Improved differentiation of switch between adding brackets and leaving brackets out

--- src/main/kotlin/nl/devhaan/kotlinpoetdsl/codeblock/SwitchCodeBlock.kt
+++ src/main/kotlin/nl/devhaan/kotlinpoetdsl/codeblock/SwitchCodeBlock.kt
@@ -1,9 +1,11 @@
 package nl.devhaan.kotlinpoetdsl.codeblock
 
+import com.squareup.kotlinpoet.CodeBlock
 import nl.devhaan.kotlinpoetdsl.CodeBlockLevel
 import nl.devhaan.kotlinpoetdsl.helpers.BlockWrapper
+import nl.devhaan.kotlinpoetdsl.helpers.createCodeBlock
 
-private fun _switch(format: String, vararg parts: Any, switchBuilder: SwitchDSL.()->Unit) = createLazyComponent{ builder->
+private fun _switch(format: String, vararg parts: Any, switchBuilder: SwitchDSL.()->Unit) = createLazyComponent(true){ builder->
     if (format == ""){
         builder.beginControlFlow("when")
     } else {
@@ -29,31 +31,42 @@ class SwitchStart(private val builder: BlockWrapper<*, *, *>) : ISwitch{
 @Suppress("unused")
 fun LazyComponentAcceptor.switch(format: String = "", vararg parts: Any, switchBuilder: SwitchDSL.() -> Unit) = _switch(format, parts, switchBuilder = switchBuilder)
 
-
 @CodeBlockLevel
 class SwitchDSL(private val builder: BlockWrapper<*, *, *>) : LazyComponentAcceptor{
-    private val cbb = CodeBlockBuilder(builder)
-
     @JvmName("thenComponent")
-    infix fun String.then(lazyComponent: LazyComponent) {
-        builder.beginControlFlow("$this ->")
-        lazyComponent.wrapper(builder)
-        builder.endControlFlow()
-    }
+    infix fun String.then(lazyComponent: LazyComponent) =
+        case(this, body = lazyComponent.toCodeBlock(), needBrackets = lazyComponent.singleStatement)
 
-    infix fun String.then(statements: CodeBlockBuilder.() -> Unit){
-        builder.beginControlFlow("$this ->")
-        cbb.statements()
-        builder.endControlFlow()
-    }
+
+    private fun CodeBlock.singleLined() = !toString().dropLast(1).contains('\n')
+    infix fun String.then(statements: CodeBlockBuilder.() -> Unit) =
+            case(this, body= createCodeBlock(statements))
+
     fun String.then(format: String, vararg parts : Any) = then{ statement(format, *parts) }
     infix fun String.then(statement: String) = then{statement.statement()}
 
-    fun case(format: String, vararg parts: Any, statements: CodeBlockBuilder.()->Unit){
-        builder.beginControlFlow("$format ->", parts)
-        cbb.statements()
-        builder.endControlFlow()
+    fun case(
+            format: String,
+            vararg parts: Any,
+            body: CodeBlock,
+            needBrackets : Boolean= body.singleLined()
+    ){
+        if (needBrackets){
+            if (parts.isEmpty()) builder.addCode("$format -> %L", body)
+            else builder.addCode("$format -> %L", arrayOf(*parts, body))
+        } else {
+            builder.beginControlFlow("$format ->", *parts)
+            builder.addCode(body)
+            builder.endControlFlow()
+        }
     }
+
+    fun case(format: String, vararg parts: Any, statements: CodeBlockBuilder.()->Unit) =
+            case(format, *parts, body = createCodeBlock(statements))
+
+    infix fun Else(lazyComponent: LazyComponent) =
+            case("else", body = lazyComponent.toCodeBlock(), needBrackets = lazyComponent.singleStatement)
+
     fun Else(statements: CodeBlockBuilder.() -> Unit) = case("else", statements = statements)
     fun Else(string: String) = Else{string.statement()}
 }
--- src/test/kotlin/nl/devhaan/kotlinpoetdsl/codeblock/SwitchTest.kt
+++ src/test/kotlin/nl/devhaan/kotlinpoetdsl/codeblock/SwitchTest.kt
@@ -1,8 +1,11 @@
 package nl.devhaan.kotlinpoetdsl.codeblock
 
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import nl.devhaan.kotlinpoetdsl.S
+import nl.devhaan.kotlinpoetdsl.files.file
 import nl.devhaan.kotlinpoetdsl.functions.createFun
 import nl.devhaan.kotlinpoetdsl.functions.func
 import nl.devhaan.kotlinpoetdsl.functions.returns
@@ -26,26 +29,70 @@ class SwitchTest : StringSpec({
         }.toString() shouldBe
                 """|fun switch() {
                    |    when(val a = Random.nextInt(10)) {
-                   |        a < 5 -> {
-                   |            println("little")
-                   |        }
-                   |        a == 5 -> {
-                   |            println("center")
-                   |        }
-                   |        a > 5 -> {
-                   |            println("big")
-                   |        }
+                   |        a < 5 -> println("little")
+                   |        a == 5 -> println("center")
+                   |        a > 5 -> println("big")
                    |    }
                    |}
                    |""".trimMargin()
     }
+
+    "switch with multiline case"{
+        createFun {
+            func("switch") {
+                switch("val a = Random.nextInt(10)") {
+                    "a < 5" then "println(\"little\")"
+                    "a == 5" then {
+                        "println(%S)".statement("center")
+                        "println(%S)".statement("center")
+                    }
+                    "a > 5".then("println(%S)", "big")
+                }
+            }
+        }.toString() shouldBe
+                """|fun switch() {
+                   |    when(val a = Random.nextInt(10)) {
+                   |        a < 5 -> println("little")
+                   |        a == 5 -> {
+                   |            println("center")
+                   |            println("center")
+                   |        }
+                   |        a > 5 -> println("big")
+                   |    }
+                   |}
+                   |""".trimMargin()
+    }
+
+
+    "importTest"{
+        file("", "HelloWorld") {
+            func("a") {
+                switch {
+                    "bigger" then {
+                        statement("%T()", String::class)
+                    }
+                    Else {
+                        statement("%T()", String::class)
+                    }
+                }
+            }
+        } shouldBe FileSpec.builder("", "HelloWorld").addFunction(
+                FunSpec.builder("a")
+                        .beginControlFlow("when")
+                        .addStatement("bigger -> %T()", String::class)
+                        .addStatement("else -> %T()", String::class)
+                        .endControlFlow()
+                        .build()
+        ).build()
+    }
+
 
     "nested switch"{
 
         createFun {
             Int::class.func("switch", "b" of Int::class, "shouldBe" of String::class) returns Boolean::class{
                 switch("shouldBe") {
-                    "bigger".S() then switch{
+                    "bigger".S() then switch {
                         "this <= b" then "false"
                         Else("true")
                     }
@@ -55,34 +102,26 @@ class SwitchTest : StringSpec({
                         Else { statement("true") }
                     }
 
-                    Else("false")
+                    this Else switch {
+                        "this >= b" then "false"
+                        Else { statement("true") }
+                    }
                 }
             }
         }.toString() shouldBe
                 """|fun kotlin.Int.switch(b: kotlin.Int, shouldBe: kotlin.String): kotlin.Boolean {
                    |    when(shouldBe) {
-                   |        "bigger" -> {
-                   |            when {
-                   |                this <= b -> {
-                   |                    false
-                   |                }
-                   |                else -> {
-                   |                    true
-                   |                }
-                   |            }
+                   |        "bigger" -> when {
+                   |            this <= b -> false
+                   |            else -> true
                    |        }
-                   |        "smaller" -> {
-                   |            when {
-                   |                this >= b -> {
-                   |                    false
-                   |                }
-                   |                else -> {
-                   |                    true
-                   |                }
-                   |            }
+                   |        "smaller" -> when {
+                   |            this >= b -> false
+                   |            else -> true
                    |        }
-                   |        else -> {
-                   |            false
+                   |        else -> when {
+                   |            this >= b -> false
+                   |            else -> true
                    |        }
                    |    }
                    |}
